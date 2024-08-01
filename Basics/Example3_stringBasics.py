06a4040eb16cc8445c35749e9f8d753ce2e771b7
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-08-02 00:00:17 +0530

    String basics

--- Basics/Example3_stringBasics.py
+++ Basics/Example3_stringBasics.py
@@ -1,3 +1,5 @@
+import re
+
 def main():
     print("Welcome to the Contact Information Formatter")
 
@@ -7,8 +9,31 @@ def main():
     phone = input("Enter your phone number: ").strip()
     address = input("Enter your address: ").strip()
 
-    # Format and Display the information
-    display_contact_info(name, email, phone, address)
+    # Validate and Format the information
+    if validate_email(email) and validate_phone(phone):
+        display_contact_info(name, email, phone, address)
+        #save_contact_info(name, email, phone, address)
+    else:
+        print("Invalid email or phone number format. Please try again!")
+
+def validate_email(email):
+    # Regular Expression for validating an Email
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(email_regex, email):
+        return True
+    return False
+
+def validate_phone(phone):
+    # String all Non-Digit characters and check length
+    digits = re.sub(r'\D', '', phone)
+    if len(digits) == 10:
+        return True
+    return False
+
+def format_phone(phone):
+    # Strip all non-digit characters and reformat
+    digits = re.sub(r'\D', '', phone)
+    return f"({digits[:3]}) {digits[3:6]}-{digits[6:]}"
 
 def display_contact_info(name, email, phone, address):
 
@@ -19,7 +44,7 @@ def display_contact_info(name, email, phone, address):
     formatted_email = email.lower()
 
     # Format the phone number(Just ensuring no leading/trailing spaces)
-    formatted_phone = phone
+    formatted_phone = format_phone(phone)
 
     # Format the address (capitalize the first letter of each word)
     formatted_address = address.title()
