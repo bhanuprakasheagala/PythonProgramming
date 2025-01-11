e5b4e91b50eb40d3a82f1596e99433ac9dcbc1aa
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2025-01-11 16:35:50 +0530

    Different types of strings

---
+++ Strings/Basics1.py
@@ -0,0 +1,9 @@
+def stringCreation():
+    single_quote_str = 'Hello, World!'
+    double_quote_str = "Hello, World!"
+    triple_quote_str = '''Hello,
+    World!'''
+    print(single_quote_str, double_quote_str, triple_quote_str)
+
+stringCreation()
+
